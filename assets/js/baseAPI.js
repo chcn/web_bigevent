99539b1516e7db41be743a0c7daaba7665f56c47
chc <father@DESKTOP-31A87EM>
2021-11-26 19:28:49 +0800

    完成了主页的一些功能

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -1,9 +1,27 @@
 $(function() {
-  // 这个函数会拦截ajax函数并获得其中的属性
-  //因为登录和注册的根路径相同，所以我可以利用这个函数拦截ajax，
-  //然后拼接url，这样就可以不用每次都填写根路径了
   $.ajaxPrefilter(function(options) {
-    console.log(options);
+    //这个函数会拦截ajax函数并获得其中的属性
+    //因为登录和注册的根路径相同，所以我可以利用这个函数拦截ajax，
+    //然后拼接url，这样就可以不用每次都填写根路径了
+    // console.log(options);
     options.url = 'http://api-breakingnews-web.itheima.net' + options.url
+    
+    //统一为需要权限的请求添加请求头headers
+    //只有带有/my/路径的请求是需要权限的
+    if(options.url.indexOf('/my/') !== -1){
+      options.headers = {
+        Authorization: localStorage.getItem('token')||''
+      }
+    }
+
+    //统一挂载complete 回调函数
+    options.complete = function(reg) {
+      // console.log(reg);
+      //当用户没有登录时，清除token并跳转到登录页面
+      if(reg.responseJSON.status === 1 && reg.responseJSON.message === '身份认证失败！') {
+        localStorage.removeItem('token')
+        location.href = '/login.html'
+      }
+    }
   })
 })
---
+++ assets/js/index.js
@@ -0,0 +1,54 @@
+$(function () {
+  //获得用户信息
+  getUserInfo()
+
+  //实现推出登录功能
+  $('#quit-btn').on('click', function () {
+    //利用layui调出提示框
+    layer.confirm('确定推出登录？', { icon: 3, title: '提示' }, function (index) {
+      //当用户点确定就会触发这个回调函数
+      //清除localStorage中的token
+      localStorage.removeItem('token')
+      //跳转登录
+      location.href = '/login.html'
+      //关闭询问框
+      layer.close(index);
+    });
+  })
+})
+
+//获得用户信息
+function getUserInfo() {
+  $.ajax({
+    method: 'GET',
+    url: '/my/userinfo',
+    // headers: {
+    //   Authorization: localStorage.getItem('token')||''
+    // },
+    success: function (reg) {
+      console.log(reg);
+      if (reg.status !== 0) return layer.msg('获取用户信息失败')
+      renderAvatar(reg.data)
+    }
+  })
+}
+
+//渲染用户名和头像
+function renderAvatar(user) {
+  var name = user.nickname || user.username
+  $('#welcome').html('欢迎&nbsp;&nbsp;' + name)
+
+  //渲染头像
+  var imgAva = $('.layui-nav-img')
+  var strAva = $('.user-avatar')
+  if (user.user_pic) {
+    //如果有图片头像,就显示图片头像，隐藏文字头像
+    strAva.hide()
+    imgAva.show()
+  } else {
+    // 没有图片头像，就渲染文字头像，隐藏图片头像，文字头像用昵称第一个字母
+    imgAva.hide()
+    strAva.html(name[0].toUpperCase()).show()
+  }
+}
+
